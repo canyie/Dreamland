eccd8f6318e5a5ddbf0d0d36e4678312c5b44fc3
canyie <a1364259@163.com>
2020-08-26 12:14:39 +0800

    Watch zygote64 instead of zygote on 64-bit devices & wait 5s for zygote start

--- template/common/service.sh
+++ template/common/service.sh
@@ -3,20 +3,26 @@ MODDIR=${0%/*}
 
 [[ -f /data/misc/dreamland/disable ]] && exit 0
 
-ZYGOTE_PID1=$(pidof zygote)
+MAIN_ZYGOTE_NICENAME=zygote
+CPU_ABI=$(getprop ro.product.cpu.api)
+[[ $CPU_ABI == "arm64-v8a" ]] && MAIN_ZYGOTE_NICENAME=zygote64
+
+sleep 5
+
+ZYGOTE_PID1=$(pidof $MAIN_ZYGOTE_NICENAME)
 sleep 20
-ZYGOTE_PID2=$(pidof zygote)
+ZYGOTE_PID2=$(pidof $MAIN_ZYGOTE_NICENAME)
 sleep 20
-ZYGOTE_PID3=$(pidof zygote)
+ZYGOTE_PID3=$(pidof $MAIN_ZYGOTE_NICENAME)
 
 [[ $ZYGOTE_PID1 == $ZYGOTE_PID2 ]] && [[ $ZYGOTE_PID2 == $ZYGOTE_PID3 ]] && exit 0
 
 sleep 20
-ZYGOTE_PID4=$(pidof zygote)
+ZYGOTE_PID4=$(pidof $MAIN_ZYGOTE_NICENAME)
 [[ $ZYGOTE_PID3 == $ZYGOTE_PID4 ]] && exit 0
 
-# Zygote keeps restarting in 60s, disable framework and restart zygote
+# Zygote keeps restarting in 65s, disable framework and restart zygote
 
 touch /data/misc/dreamland/disable
-echo "Bootloop protection: zygote keeps restarting in 60s" >> /data/misc/dreamland/disable_reason
+echo "Bootloop protection: zygote keeps restarting in 65s" >> /data/misc/dreamland/disable_reason
 setprop ctl.restart zygote
