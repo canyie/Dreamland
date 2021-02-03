11e730a52f41b727551e63f0f50a517693d0fe9c
canyie <a1364259@163.com>
2021-02-03 15:33:24 +0800

    Multi-language support during installation

--- template/customize.sh
+++ template/customize.sh
@@ -4,16 +4,21 @@ RIRU_NEW_PATH=/data/adb/riru
 RIRU_NEW_MODULE_PATH=$RIRU_NEW_PATH/modules/dreamland
 DREAMLAND_PATH=/data/misc/dreamland
 
+ui_print "- Loading languages"
+unzip -o "$ZIPFILE" languages.sh -d "$TMPDIR" >&2
+[[ -f "$TMPDIR/languages.sh" ]] || abort "! Unable to extract languages.sh"
+. "$TMPDIR"/languages.sh
+
 if [[ "$ARCH" != "arm64" && "$ARCH" != "arm" ]]; then
-  abort "! Unsupported architecture: $ARCH"
+  abort "! $ERR_UNSUPPORTED_ARCH $ARCH"
 else
-  ui_print "- Device architecture: $ARCH"
+  ui_print "- $ALERT_ARCH $ARCH"
 fi
 
 if [[ "$API" -lt 24 ]]; then
-  abort "! Unsupported Android API level $API"
+  abort "! $ERR_UNSUPPORTED_ANDROID_API $API"
 else
-  ui_print "- Android API level: $API"
+  ui_print "- $ALERT_ANDROID_API $API"
 fi
 
 if [[ -f "$RIRU_OLD_PATH/api_version.new" || -f "$RIRU_OLD_PATH/api_version" ]]; then
@@ -23,57 +28,57 @@ elif [[ -f "$RIRU_NEW_PATH/api_version.new" || -f "$RIRU_NEW_PATH/api_version" ]
   RIRU_PATH="$RIRU_NEW_PATH"
   RIRU_MODULE_PATH="$RIRU_NEW_MODULE_PATH"
 else
-  abort "! Requirement module 'Riru - Core' is not installed"
+  abort "! $ERR_RIRU_NOT_INSTALLED"
 fi
 
 RIRU_API_VERSION=$(cat "$RIRU_PATH/api_version.new") || RIRU_API_VERSION=$(cat "$RIRU_PATH/api_version") || RIRU_API_VERSION=0
-ui_print "- Riru API version: $RIRU_API_VERSION"
+ui_print "- $ALERT_RIRU_API $RIRU_API_VERSION"
 
 RIRU_MIN_API=$(grep_prop api "$TMPDIR/module.prop")
-[[ "$RIRU_API_VERSION" -ge "$RIRU_MIN_API" ]] || abort "! Unsupported Riru API version $RIRU_API_VERSION"
+[[ "$RIRU_API_VERSION" -ge "$RIRU_MIN_API" ]] || abort "! $ERR_UNSUPPORTED_RIRU_API $RIRU_API_VERSION"
 
 if [[ "${BOOTMODE}" == "true" ]]; then
   if [[ $(pm path "top.canyie.dreamland.manager") == "" ]]; then
     if [[ $(pm path "top.canyie.dreamland.manager") != "" ]]; then
-      ui_print "- Detected deprecated dreamland manager"
-      ui_print "- It is not compatible with current framework version"
+      ui_print "- $WARN_OLD_MANAGER_1"
+      ui_print "- $WARN_OLD_MANAGER_2"
     else
-      ui_print "- Dreamland Manager not found"
-      ui_print "- You cannot manage Dreamland configurations"
+      ui_print "- $WARN_MANAGER_NOT_INSTALLED_1"
+      ui_print "- $WARN_MANAGER_NOT_INSTALLED_2"
     fi
-    ui_print "- Please install new Dreamland Manager"
+    ui_print "- $WARN_PLEASE_INSTALL_NEW_MANAGER"
   fi
 fi
 
-ui_print "- Extracting module files"
-unzip -oj "$ZIPFILE" module.prop uninstall.sh 'common/*' -d "$MODPATH" >&2 || abort "! Can't extract module files: $?"
-unzip -o "$ZIPFILE" 'system/*' -d "$MODPATH" >&2 || abort "! Can't extract the system folder: $?"
+ui_print "- $ALERT_EXTRACT_MODULE_FILES"
+unzip -oj "$ZIPFILE" module.prop uninstall.sh 'common/*' -d "$MODPATH" >&2 || abort "! $ERR_EXTRACT_MODULE_FILES $?"
+unzip -o "$ZIPFILE" 'system/*' -d "$MODPATH" >&2 || abort "! $ERR_EXTRACT_SYSTEM_FOLDER $?"
 
 if [[ "$IS64BIT" == "false" ]]; then
-  ui_print "- Removing 64-bit libraries"
+  ui_print "- $ALERT_REMOVE_LIB64"
   rm -rf "$MODPATH/system/lib64"
 fi
 
-ui_print "- Extracting riru files"
-[[ -d "$RIRU_MODULE_PATH" ]] || mkdir -p "$RIRU_MODULE_PATH" || abort "! Can't create $RIRU_MODULE_PATH: $?"
-cp -f "$TMPDIR/module.prop" "$RIRU_MODULE_PATH/module.prop" || abort "! Can't copy module.prop to $RIRU_MODULE_PATH: $?"
+ui_print "- $ALERT_EXTRACT_RIRU_FILES"
+[[ -d "$RIRU_MODULE_PATH" ]] || mkdir -p "$RIRU_MODULE_PATH" || abort "! $ERR_CREATE_RIRU_MODULE_PATH $?"
+cp -f "$TMPDIR/module.prop" "$RIRU_MODULE_PATH/module.prop" || abort "! $ERR_COPY_PROP_TO_RIRU_MODULE_PATH $?"
 
-ui_print "- Preparing local directory"
-[[ -d "$DREAMLAND_PATH" ]] || mkdir -p "$DREAMLAND_PATH" || abort "! Can't create $DREAMLAND_PATH: $?"
+ui_print "- $ALERT_PREPARE_LOCAL_DIR"
+[[ -d "$DREAMLAND_PATH" ]] || mkdir -p "$DREAMLAND_PATH" || abort "! $ERR_PREPARE_LOCAL_DIR $?"
 
 if [[ "$MAGISK_VER_CODE" -lt 20200 ]]; then
-  ui_print "- Removing sepolicy because of Magisk version $MAGISK_VER is lower than 20.2"
-  ui_print "- We recommend that you upgrade Magisk to the latest version"
+  ui_print "- $ALRET_REMOVE_SEPOLICY_1"
+  ui_print "- $ALRET_REMOVE_SEPOLICY_2"
   rm -f "$MODPATH/sepolicy.rule"
 fi
 
 # before Magisk 16e4c67, sepolicy.rule is copied on the second reboot
 if [[ "$MAGISK_VER_CODE" -lt 21006 ]]; then
-  ui_print "- Magisk version below 21006"
-  ui_print "- You may need to manually reboot twice"
+  ui_print "- $ALERT_REBOOT_TWICE_1"
+  ui_print "- $ALERT_REBOOT_TWICE_2"
 fi
 
-ui_print "- Setting permissions"
+ui_print "- $ALERT_SETTING_PERMISSIONS"
 # The following is the default rule, DO NOT remove
 set_perm_recursive "$MODPATH" 0 0 0755 0644
 set_perm_recursive "$DREAMLAND_PATH" 1000 1000 0700 0600 u:object_r:system_data_file:s0
---
+++ template/languages.sh
@@ -0,0 +1,67 @@
+SIMPLIFIED_CHINESE=false
+if [[ "$BOOTMODE" == "true" ]]; then
+  locale=$(getprop persist.sys.locale|awk -F "-" '{print $1"_"$NF}')
+  [[ "$locale" == "" ]] && locale=$(settings get system system_locales|awk -F "," '{print $1}'|awk -F "-" '{print $1"_"$NF}')
+  [[ "$locale" == "zh_CN" ]] && SIMPLIFIED_CHINESE=true
+fi
+
+if [[ "$SIMPLIFIED_CHINESE" == "true" ]]; then
+  ALERT_ARCH="设备架构："
+  ALERT_ANDROID_API="Android API级别："
+  ALERT_RIRU_API="Riru API版本："
+  ALERT_EXTRACT_MODULE_FILES="正在解压模块文件"
+  ALERT_REMOVE_LIB64="正在移除64位支持库"
+  ALERT_EXTRACT_RIRU_FILES="正在初始化Riru模块文件"
+  ALERT_PREPARE_LOCAL_DIR="正在准备配置目录"
+  ALRET_REMOVE_SEPOLICY_1="Magisk版本低于20.2，正在移除sepolicy.rule"
+  ALRET_REMOVE_SEPOLICY_2="我们建议您升级Magisk"
+  ALERT_REBOOT_TWICE_1="Magisk版本低于21006"
+  ALERT_REBOOT_TWICE_2="您可能需要重启设备两次才能工作"
+  ALERT_SETTING_PERMISSIONS="正在设置文件权限"
+
+  ERR_UNSUPPORTED_ARCH="不支持的设备架构："
+  ERR_UNSUPPORTED_ANDROID_API="不支持的Android API级别："
+  ERR_RIRU_NOT_INSTALLED="请先安装模块 'Riru'"
+  ERR_UNSUPPORTED_RIRU_API="不支持的 Riru API版本："
+  ERR_EXTRACT_MODULE_FILES="解压模块文件失败："
+  ERR_EXTRACT_SYSTEM_FOLDER="解压system目录失败："
+  ERR_CREATE_RIRU_MODULE_PATH="创建riru模块路径失败："
+  ERR_COPY_PROP_TO_RIRU_MODULE_PATH="复制module.prop失败："
+  ERR_PREPARE_LOCAL_DIR="无法创建配置目录："
+
+  WARN_OLD_MANAGER_1="您安装了已弃用的旧版管理器"
+  WARN_OLD_MANAGER_2="它与您正在安装的梦境框架不兼容"
+  WARN_MANAGER_NOT_INSTALLED_1="梦境管理器未安装"
+  WARN_MANAGER_NOT_INSTALLED_2="您将无法管理框架设置"
+  WARN_PLEASE_INSTALL_NEW_MANAGER="请安装新版管理器！"
+else
+  ALERT_ARCH="Device architecture:"
+  ALERT_ANDROID_API="Android API level:"
+  ALERT_RIRU_API="Riru API version:"
+  ALERT_EXTRACT_MODULE_FILES="Extracting module files"
+  ALERT_REMOVE_LIB64="Removing 64-bit libraries"
+  ALERT_EXTRACT_RIRU_FILES="Extracting riru files"
+  ALERT_PREPARE_LOCAL_DIR="Preparing local directory"
+  ALRET_REMOVE_SEPOLICY_1="Removing sepolicy because of your Magisk version is lower than 20.2"
+  ALRET_REMOVE_SEPOLICY_2="We recommend that you upgrade Magisk to the latest version"
+  ALERT_REBOOT_TWICE_1="Magisk version below 21006"
+  ALERT_REBOOT_TWICE_2="You may need to manually reboot twice"
+  ALERT_SETTING_PERMISSIONS="Setting permissions"
+
+  ERR_UNSUPPORTED_ARCH="Unsupported architecture:"
+  ERR_UNSUPPORTED_ANDROID_API="Unsupported Android API level"
+  ERR_RIRU_NOT_INSTALLED="Requirement module 'Riru' is not installed"
+  ERR_UNSUPPORTED_RIRU_API="Unsupported Riru API version"
+  ERR_EXTRACT_MODULE_FILES="Can't extract module files:"
+  ERR_EXTRACT_SYSTEM_FOLDER="Can't extract the system folder:"
+  ERR_CREATE_RIRU_MODULE_PATH="Can't create riru module path:"
+  ERR_COPY_PROP_TO_RIRU_MODULE_PATH="Can't copy module.prop to the riru module path:"
+  ERR_PREPARE_LOCAL_DIR="Can't create the local directory:"
+
+  WARN_OLD_MANAGER_1="Detected deprecated dreamland manager"
+  WARN_OLD_MANAGER_2="It is not compatible with current framework version"
+  WARN_MANAGER_NOT_INSTALLED_1="Dreamland Manager not found"
+  WARN_MANAGER_NOT_INSTALLED_2="You cannot manage Dreamland configurations"
+  WARN_PLEASE_INSTALL_NEW_MANAGER="Please install new Dreamland Manager"
+fi
+
