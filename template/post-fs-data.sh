228d19f3a86d2c5f5a241285a168dfdbfbade057
canyie <a1364259@163.com>
2021-04-09 21:15:54 +0800

    No need to allow zygote execmem

--- template/post-fs-data.sh
+++ template/post-fs-data.sh
@@ -4,7 +4,6 @@ MODDIR=${0%/*}
 
 [ -f "${MODDIR}/sepolicy.rule" ] && exit 0
 
-magiskpolicy --live "allow zygote zygote process { execmem }" \
-    "allow system_server system_server process { execmem }" \
+magiskpolicy --live "allow system_server system_server process { execmem }" \
     "allow system_server apk_data_file file *" \
     "allow system_server app_data_file file *"
