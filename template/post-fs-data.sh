a23eb6f6d18e5bd3e5f12359b466f81109898b69
canyie <a1364259@163.com>
2024-02-05 11:52:05 +0800

    Enforce stricter selinux rules

--- template/post-fs-data.sh
+++ template/post-fs-data.sh
@@ -4,6 +4,4 @@ MODDIR=${0%/*}
 
 [ -f "${MODDIR}/sepolicy.rule" ] && exit 0
 
-magiskpolicy --live "allow system_server system_server process { execmem }" \
-    "allow system_server apk_data_file file *" \
-    "allow system_server app_data_file file *"
+magiskpolicy --live "allow system_server system_server process { execmem }"
