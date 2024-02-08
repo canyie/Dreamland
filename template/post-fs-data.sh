177971cd67dc08a45f189b2ab79e1eb23e5d14c3
canyie <a1364259@163.com>
2024-02-08 22:47:02 +0800

    Allow system_server to load native libs from untrusted app
    Previously allowed but removed in a recent change.
    This is required for modules to load their native libraries in system_server, but isn't supported by LSPosed due to "security concerns and it's an useless feature". Another behavior difference. So amazing.

--- template/post-fs-data.sh
+++ template/post-fs-data.sh
@@ -4,4 +4,5 @@ MODDIR=${0%/*}
 
 [ -f "${MODDIR}/sepolicy.rule" ] && exit 0
 
-magiskpolicy --live "allow system_server system_server process { execmem }"
+magiskpolicy --live "allow system_server system_server process execmem" \
+"allow system_server apk_data_file file execute"
